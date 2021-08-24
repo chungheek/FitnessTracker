a2b5b18c009c05d42ff28e04adefa21688a9f3df
Chunghee Kim <chungheek@gmail.com>
2021-08-24 10:53:38 -0400

    Replaced node package mysql with mariadb and updated sql file

--- back-end/db/db.sql
+++ back-end/db/db.sql
@@ -1,10 +1,14 @@
-
 CREATE TABLE exercise (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) UNIQUE,
+  name VARCHAR(255),
   reps INT,
   weight INT,
   unit VARCHAR(255),
   date DATETIME DEFAULT NOW(),
   PRIMARY KEY (id)
 );
+INSERT INTO exercise (name, reps, weight, unit)
+VALUES ("bench", 10, 185, "lbs"),
+  ("deadlift", 10, 300, "lbs"),
+  ("rows", 5, 100, "lbs"),
+  ("powerlift", 8, 150, "kg");
