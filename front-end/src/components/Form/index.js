9a648f18b6145a99896a4f005444bfb25b6e107f
Chunghee Kim <chungheek@gmail.com>
2021-08-27 18:40:43 -0400

    Created Form component

---
+++ front-end/src/components/Form/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Form.scss";
+
+const Form = () => {
+  const [data, setData] = useState(null);
+
+  const getData = () => {
+    try {
+      axios.get("http://locallhost:3000").then((res) => {
+        if (res.status === 200) {
+          console.log(res.data);
+          setData(res.data);
+        }
+      });
+    } catch (err) {
+      console.log(`Issue with GET call: ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [setData]);
+
+  return (
+    <form className="App-form">
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Reps:
+        <input type="text" name="reps" />
+      </label>
+      <label>
+        Weight:
+        <input type="text" name="weight" />
+      </label>
+      <label>
+        Unit:
+        <input type="text" name="unit" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
+
+export default Form;
