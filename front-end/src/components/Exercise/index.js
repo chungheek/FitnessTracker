1097231ffd73a8e29c86dd4c84861243c8c68e4d
Chunghee Kim <chungheek@gmail.com>
2021-08-27 18:41:11 -0400

    Created Exercise component

---
+++ front-end/src/components/Exercise/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Exercise.scss";
+
+const Exercise = () => {
+  return (
+    <div className="ex">
+      <div>name</div>
+      <div>reps</div>
+      <div>weight</div>
+      <div>unit</div>
+    </div>
+
+  );
+};
+
+export default Exercise;
